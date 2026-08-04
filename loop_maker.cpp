5cfb2aea8439aee7aa311eacb1c35fc5c33848e9
rd25-23 <rd0519@srmist.edu.in>
2026-08-04 23:35:04 +0530

    added pattern 10,pattern 10 2nd version and pattern 11,pattern 11 2nd version

--- loop_maker.cpp
+++ loop_maker.cpp
@@ -126,6 +126,7 @@ void pattern9()
     int n;
     cout << "Enter the value of n for pattern 9:";
     cin >> n;
+    // we should have called pattern8() and pattern9() here instead
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j < n - i; j++)
@@ -151,8 +152,108 @@ void pattern9()
         cout << "\n";
     }
 }
+void pattern10()
+{
+    int n;
+    cout << "Enter the value of n for pattern 10:";
+    cin >> n;
+    // my way;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+void pattern10_2ndway()
+{
+    int n;
+    cout << "Enter the value of n for pattern 10 2nd version:";
+    cin >> n;
+    /* not good method we have to use if, define a new variable , think extra and
+    still big o is same as the 1st way which is way easier to think and write and
+    understand*/
+
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int star = i;
+        if (i > n)
+        {
+            star = 2 * n - i;
+        }
+        for (int j = 1; j <= star; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+void pattern11()
+{
+    int n;
+    cout << "Enter the value of n for pattern 11:";
+    cin >> n;
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            start = 1;
+        }
+        else
+        {
+            start = 0;
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << "\n";
+    }
+}
+void pattern11_2ndway()
+{
+    int n;
+    cout << "Enter the value of n for pattern 11 2nd version:";
+    cin >> n;
+    string fin, r = "1", p = "0";
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            fin = r + fin;
+        }
+        else
+        {
+            fin = p + fin;
+        }
+        cout << fin << "\n";
+    }
+}
 int main()
 {
-
+    pattern1();
+    pattern2();
+    pattern3();
+    pattern4();
+    pattern5();
+    pattern6();
+    pattern7();
+    pattern8();
     pattern9();
+    pattern10();
+    pattern10_2ndway();
+    pattern11();
+    pattern11_2ndway();
 }
